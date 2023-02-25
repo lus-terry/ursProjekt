e218baf0b21520ef49d0457bf2b7b93f70822682
turatotoni <124459324+turatotoni@users.noreply.github.com>
2023-02-25 14:30:49 +0100

    Add files via upload

---
+++ ursProjektKeypad/timerZaSekunde.h
@@ -0,0 +1,16 @@
+/*
+ * timerZaSekunde.h
+ *
+ * Created: 24.2.2023. 10:50:14
+ *  Author: Korisnik
+ */ 
+
+
+#ifndef TIMERZASEKUNDE_H_
+#define TIMERZASEKUNDE_H_
+
+int main(void);
+
+
+
+#endif /* TIMERZASEKUNDE_H_ */
