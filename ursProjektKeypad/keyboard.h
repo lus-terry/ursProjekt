74231a1742ba4ac227a67d994a0e6abea397a5f7
turatotoni <124459324+turatotoni@users.noreply.github.com>
2023-02-25 02:06:11 +0100

    Add files via upload

---
+++ ursProjektKeypad/keyboard.h
@@ -0,0 +1,16 @@
+/*
+ * IncFile1.h
+ *
+ * Created: 22.2.2023. 18:54:15
+ *  Author: Lucija
+ */ 
+
+
+#ifndef INCFILE1_H_
+#define INCFILE1_H_
+
+ char pressedKey();
+
+
+
+#endif /* INCFILE1_H_ */
